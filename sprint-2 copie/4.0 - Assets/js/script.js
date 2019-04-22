917c1c4169efb728ab4ff68350ef4c0d341e0b6e
Marion Cointre <marion.cointre@gmail.com>
2019-04-21 22:05:51 -0700

    final submission

--- sprint-2 copie/4.0 - Assets/js/script.js
+++ sprint-2 copie/4.0 - Assets/js/script.js
@@ -50,26 +50,6 @@ today = dd + '/' + mm + '/' + yyyy;
 
 // Add new information to a new comment Object
 function createComment (){
-/*    commentsTable = [
-        {
-        nameIcon: '',
-        commentDate:'12/18/2018',
-        userName:'Micheal Lyons',
-        commentInput:'They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.'
-        },
-        {
-        nameIcon: '',
-        commentDate:'12/18/2018',
-        userName:'Gary Wong',
-        commentInput:'Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!'
-        },
-        {
-        nameIcon: '',
-        commentDate:'11/15/2018',
-        userName:'Theodore Duncan',
-        commentInput:'How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!'
-        }];
-*/
         newCommentName = document.querySelector('#Name').value;
         newComment.userName = newCommentName;
         newCommentInput = document.querySelector('#Comment').value;
@@ -137,11 +117,8 @@ document.getElementById("Btn").addEventListener("click", submit)
 function submit(event) {
     // prevent the webpage from reloading
         event.preventDefault();
-    // declare variables
     createComment();
-    // table.classList.add('hidden');
     document.querySelector('.comments-table').innerHTML = '';
     displayComments();
-    // table.classList.remove('hidden');
     document.getElementById("myForm").reset();
     }
