e1885286881a4b877ea7ac0892b72f6878f14363
Marion Cointre <marion.cointre@gmail.com>
2019-04-25 16:53:57 -0700

    saving progress

--- sprint-3/assets/js/shows2.js
+++ sprint-3/assets/js/shows2.js
@@ -14,40 +14,34 @@ let showArr = axios.get(
 
 showArr.then(function(response) {
   let showArrRes = response.data;
-
   console.log(showArrRes);
 
-  for (var i = 0; i < showArrRes.length; i++) {
-    var pDate = document.createElement("p");
-    var columnDate = document.createElement("td");
-    // var button = <button>Buy Tickets</button>;
-    // var column3 = document.createElement("td");
-    // var paragraph = document.createElement("p");
-    // var column2 = document.createElement("td");
-    // var title5 = document.createElement("H5");
-    // var column4 = document.createElement("td");
-    var row = document.createElement("tr");
+  for (let i = 0; i < showArrRes.length; i++) {
+    // creating the basic elements
+    let pLocation = document.createElement("p");
+    let columnLocation = document.createElement("td");
+    let pVenue = document.createElement("p");
+    let columnVenue = document.createElement("td");
+    let pDate = document.createElement("p");
+    let columnDate = document.createElement("td");
+    let row = document.createElement("tr");
     let showTable = document.querySelector(".shows-table");
 
-    // declare variables
+    let showDates = showArrRes[i].date;
+    let showLocation = response.data[i].location;
+    let showVenue = response.data[i].place;
 
-    var showDates = response.data[i].date;
-    // var showLocation = response.data[i].location;
-    // var showVenue = response.data[i].place;
+    pLocation.innerHTML = showLocation; // edit pararapgrah to add show data
+    columnLocation.appendChild(pLocation); // appending column to add paragraph
+    row.appendChild(columnLocation); // appending row to add column
 
-    showTable.innerText = row; //edit body to add row tag
-    row.innerText = columnDate; // edit row to add column
-    columnDate.innerText = pDate; // edit column to add paragraph
-    pDate.innerText = showDates; // edit pararapgrah to add show data
+    pVenue.innerHTML = showVenue; // edit pararapgrah to add show data
+    columnVenue.appendChild(pVenue); // appending column to add paragraph
+    row.appendChild(columnVenue); // appending row to add column
 
-    // column1.appendChild(button);
-    // column2.appendChild(title5);
-    // column3.appendChild(paragraph);
-    // paragraph.appendChild(showLocation);
-
-    showTable.appendChild(row); //apending body to add row
-    row.appendChild(columnDate); // apending row to add column
-    columnDate.appendChild(pDate); // apending column to add paragraph
-    pDate.appendChild(showDates); // apending paragraph to add data
+    pDate.innerHTML = showDates; // edit pararapgrah to add show data
+    columnDate.appendChild(pDate); // appending column to add paragraph
+    row.appendChild(columnDate); // appending row to add column
+    showTable.appendChild(row); //appending body to add row
   }
 });
